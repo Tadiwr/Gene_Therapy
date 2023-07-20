cde9ed971a6b096c42a765b228d6d831537fd560
Tadiwanashe Shangwa <tshxng@gmail.com>
2023-07-20 09:18:36 +0200

    Added wiki files

---
+++ assets/wikis/phases.md
@@ -0,0 +1,3 @@
+# What are Phases
+
+refers to
--- pages/Wiki📖.py
+++ pages/Wiki📖.py
@@ -1,12 +1,40 @@
 import streamlit as st
 
+choice = ""
+
+
+options = [
+    "Phases",
+    "Therapuetic Areas",
+    "Therapuetic Routes",
+    "Drugs",
+    "Points",
+    "Vectors",
+    "Trial Status",
+    "Efficacy",
+    "Gene Therapy"
+]
+
+def readFile(choice: str) -> str :
+    fileName : str = f"./assets/wikis/{choice.lower()}.md"
+    text : str = ""
+
+    file = open(fileName, "r")
+    text = file.read()
+
+    return text
+
+
 "# Wiki 📖"
 
 "## Dont Understand Somthing?"
 
-options = []
-
-st.selectbox(
+choice = st.selectbox(
     label="What dont you understand?",
     options=options
 )
+
+st.write(readFile(choice))
+
+
+
